05a78033bd01057589f31d23cd6c63c28000cfc6
SachinUjjainkar <Sachin.ujjainkar@anchanto.com>
2019-12-19 10:20:09 +0530

    New Branch Commit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,6 +21,7 @@ class ArticlesController < ApplicationController
     def create
         #render plain:params[:article].inspect
         @article=Article.new(article_params)
+        @article.user=User.first
         if @article.save
             flash[:notice] = "Saved sucessfully"
             redirect_to article_path(@article)
