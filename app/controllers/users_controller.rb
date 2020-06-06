604cb4f1ae676bdd83191f7f6765f99e6a84881b
megha1302 <megha.sonkusare@anchanto.com>
2020-06-06 19:44:25 +0530

    show user changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,8 @@ class UsersController < ApplicationController
     # GET /users/1
     # GET /users/1.json
     def show
+       @user = User.find(params[:id])
+       @articles = @user.articles
     end
 
     # GET /users/new
