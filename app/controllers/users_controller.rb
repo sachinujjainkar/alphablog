0990cac1822b715b9aea8e7fa6124a3c04e50265
megha1302 <megha.sonkusare@anchanto.com>
2020-06-07 16:38:10 +0530

    Image option added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
     # GET /users/1.json
     def show
        @user = User.find(params[:id])
-       @articles = @user.articles
+       @articles = @user.articles.paginate(:page=>params[:page],per_page:2)
     end
 
     # GET /users/new
