55552b5ce13c9062f9088484035d3635b556610f
Harshada-1996 <harshada.chinchole@anchanto.com>
2020-06-13 18:46:04 +0530

    update userindex

--- db/migrate/20200613123218_add_columns_to_user.rb
+++ db/migrate/20200613123218_add_columns_to_user.rb
@@ -1,6 +1,6 @@
 class AddColumnsToUser < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :DOB, :string
+    add_column :users, :DOB, :date
     add_column :users, :Gender, :string
     add_column :users, :Course, :string
     add_column :users, :Bio, :string
